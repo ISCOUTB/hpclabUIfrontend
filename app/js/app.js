38ba9e3a16ead1c1f6d8868bf182a81d723437ad
Hernando Ariza Perez <locoariza17@gmail.com>
2015-09-23 16:29:05 -0500

    adicionando pagina HOME de prueba

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,17 @@
 var app = angular.module('app', ['ngRoute']);
 
 var configuracion = function ($routeProvider) {
-   
+
    $routeProvider.when('/', {
       templateUrl: 'partials/login.html',
-      controller:'loginController'
-   }).otherwise({
+      controller: 'loginController'
+   });
+   
+   $routeProvider.when('/home', {
+      templateUrl: 'partials/home.html'
+   });
+   
+   $routeProvider.otherwise({
       redirecTo: '/'
    });
 };
--- app/js/services/loginService.js
+++ app/js/services/loginService.js
@@ -1,18 +1,22 @@
 'use strict';
 
-app.factory('loginService', function ($http) {
+app.factory('loginService', function ($http, $window) {
 
    return {
       login: function (user) {
-         var $url=$http.post("http://api.hpclab.unitecnologica.edu.co/api-token-auth/",user);
+         var $url = $http.post("http://api.hpclab.unitecnologica.edu.co/api-token-auth/", user);
          //var $url=$http.post("data/user.php",user);
-         
-         $url.then(function (msj){
-            
-            if(msj.data=='exito'){
-               console.log('SIIIII');
-            }else{
-               console.log('NOOOOO');
+
+         $url.then(function (msj) {
+
+            if (msj.status == 200) {
+               console.log(msj.data);
+               $window.location.href = '/home';
+            } else {
+               $window.location.href = '/error';
+               console.log(msj.data);
+
+
             }
          });
       }
