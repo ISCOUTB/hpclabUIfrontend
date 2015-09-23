bd4f62ad19974a20baafaaf18693d0b0bbef2db9
Hernando Ariza Perez <locoariza17@gmail.com>
2015-09-23 15:57:33 -0500

    cambiando uri

--- app/js/services/loginService.js
+++ app/js/services/loginService.js
@@ -4,7 +4,7 @@ app.factory('loginService', function ($http) {
 
    return {
       login: function (user) {
-         var $url=$http.post("http://172.16.9.170:9000/api-token-auth/",user);
+         var $url=$http.post("http://api.hpclab.unitecnologica.edu.co/api-token-auth/",user);
          //var $url=$http.post("data/user.php",user);
          
          $url.then(function (msj){
