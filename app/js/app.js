bbafdd1c243c11e996bb1f5cba6cc7209429d584
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-09-24 23:01:25 -0500

    Cambios en el archivo inicial de app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,3 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute']);
-
-var configuracion = function ($routeProvider) {
-
-   $routeProvider.when('/', {
-      templateUrl: 'partials/login.html',
-      controller: 'loginController'
-   });
-   
-   $routeProvider.when('/home', {
-      templateUrl: 'partials/home.html'
-   });
-   
-    $routeProvider.when('/error', {
-      templateUrl: 'partials/error.html'
-   });
-   
-   $routeProvider.otherwise({
-      redirecTo: '/'
-   });
-};
-
-app.config(configuracion);
+var app = angular.module('app', ['ui.router']);
