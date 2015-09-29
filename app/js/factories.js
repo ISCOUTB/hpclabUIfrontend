e9dcf1f4cf7579dbfdd9a0c7ae25296cf2283abb
Hernando Ariza Perez <locoariza17@gmail.com>
2015-09-29 10:23:53 -0500

    Agregando JWT angular auth

--- app/js/factories.js
+++ app/js/factories.js
@@ -1,87 +1,87 @@
 'use strict';
 (function () {
-	var app = angular.module('app');
+   var app = angular.module('app');
 
-	/**
-	 * Factory Http Error Response Interceptor
-	 */
+   /**
+    * Factory Http Error Response Interceptor
+    */
 
-	app.factory('httpErrorResponseInterceptor', ['$q', '$location', '$localStorage',
+   app.factory('httpErrorResponseInterceptor', ['$q', '$location', '$localStorage',
 		function ($q, $location, $localStorage) {
-			return {
-				request       : function (config) {
-					config.headers = config.headers || {};
-					if ($localStorage.token) {
-						config.headers.Authorization = 'Bearer' + $localStorage.token;
-					}
-					return config;
-				},
-				response      : function (responseData) {
-					return responseData;
-				},
-				responseError : function error(response) {
-					switch (response.status) {
-						case 401:
-							$location.path('/login');
-							break;
-						case 404:
-							$location.path('/404');
-							break;
-					}
+         return {
+            request: function (config) {
+               config.headers = config.headers || {};
+               if ($localStorage.token) {
+                  config.headers.Authorization = 'Bearer' + $localStorage.token;
+               }
+               return config;
+            },
+            response: function (responseData) {
+               return responseData;
+            },
+            responseError: function error(response) {
+               switch (response.status) {
+               case 401:
+                  $location.path('/login');
+                  break;
+               case 404:
+                  $location.path('/404');
+                  break;
+               }
 
-					return $q.reject(response);
-				}
-			};
+               return $q.reject(response);
+            }
+         };
 		}
 	]);
 
-	/*
-	 *  Factory Authentication
-	 */
+   /*
+    *  Factory Authentication
+    */
 
-	app.factory('Auth', ['$http', '$localStorage', function ($http, $localStorage, urls) {
-		function urlBase64Decode(str) {
-			var output = str.replace('-', '+').replace('_', '/');
-			switch (output.length % 4) {
-				case 0:
-					break;
-				case 2:
-					output += '==';
-					break;
-				case 3:
-					output += '=';
-					break;
-				default:
-					throw 'Illegal base64url string!';
-			}
-			return window.atob(output);
-		}
+   app.factory('Auth', ['$http', '$localStorage', function ($http, $localStorage, urls) {
+      function urlBase64Decode(str) {
+         var output = str.replace('-', '+').replace('_', '/');
+         switch (output.length % 4) {
+         case 0:
+            break;
+         case 2:
+            output += '==';
+            break;
+         case 3:
+            output += '=';
+            break;
+         default:
+            throw 'Illegal base64url string!';
+         }
+         return window.atob(output);
+      }
 
-		function getClaimsFromToken() {
-			var token = $localStorage.token;
-			var user = {};
-			if (typeof token !== 'undefined') {
-				var encoded = token.split('.')[1];
-				user = JSON.parse(urlBase64Decode(encoded));
-			}
-			return user;
-		}
+      function getClaimsFromToken() {
+         var token = $localStorage.token;
+         var user = {};
+         if (typeof token !== 'undefined') {
+            var encoded = token.split('.')[1];
+            user = JSON.parse(urlBase64Decode(encoded));
+         }
+         return user;
+      }
 
-		var tokenClaims = getClaimsFromToken();
+      var tokenClaims = getClaimsFromToken();
 
-		return {
-			login          : function (data, success, error) {
-				$http.post('/login', data).success(success).error(error)
-			},
-			logout         : function (success) {
-				tokenClaims = {};
-				delete $localStorage.token;
-				success();
-			},
-			getTokenClaims : function () {
-				return tokenClaims;
-			}
-		};
+      return {
+         login: function (data, success, error) {
+            $http.post('/login', data).success(success).error(error)
+         },
+         logout: function (success) {
+            tokenClaims = {};
+            delete $localStorage.token;
+            success();
+         },
+         getTokenClaims: function () {
+            return tokenClaims;
+         }
+      };
 	}]);
 
 
--- app/js/modules/login.js
+++ app/js/modules/login.js
@@ -1,25 +1,45 @@
 'use strict';
 
 (function () {
-	var login = angular.module('loginmodule', []);
+   var login = angular.module('loginmodule', []);
 
-	login.controller('loginController', function ($scope, loginService) {
-		$scope.login = function (user) {
-			loginService.login(user);
-		}
-	});
+   login.controller('loginController', function ($scope, jwt_loginService) {
+      $scope.login = function (user) {
+         jwt_loginService.login(user).then(function (result) {
+            var token = JSON.stringify(response.data);
+            if (!window.localStorage.getItem("token")) {
 
-	login.factory('loginService', function ($http, $location) {
-		return {
-			login : function (user) {
-				var $promise = $http.post("https://api.hpclab.unitecnologica.edu.co/api-token-auth/", user);
-				$promise.then(function (response) {
-					$location.path('/home');
-					$location.replace();
-				}, function (response) {
-					user.error = true;
-				});
-			}
-		}
-	});
+               window.localStorage.setItem("token", token);
+            }
+            $location.path('/home');
+
+         });
+      }
+   });
+
+   login.factory('jwt_loginService', function ($http, $location) {
+      var loginSvc = {};
+      loginSvc.login = function (user) {
+         return $http({
+            method: "POST",
+            url: "https://api.hpclab.unitecnologica.edu.co/api-token-auth/",
+            data: user
+         });
+      }
+      return loginSvc;
+   });
+
+   login.factory('loginService', function ($http, $location) {
+      return {
+         login: function (user) {
+            var $promise = $http.post("https://api.hpclab.unitecnologica.edu.co/api-token-auth/", user);
+            $promise.then(function (response) {
+               $location.path('/home');
+               $location.replace();
+            }, function (response) {
+               user.error = true;
+            });
+         }
+      }
+   });
 })();
