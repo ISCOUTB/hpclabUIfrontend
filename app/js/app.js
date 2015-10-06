4cd38830c8bff6713002d3918b2997d7247bcfb6
Anheru88 <ajimenez@digitalcaribe.com.co>
2015-10-06 14:59:10 -0500

    Comenzando a trabajar con angular materialize, en los archivos de inicio

--- app/js/app.js
+++ app/js/app.js
@@ -1,61 +1,67 @@
 'use strict';
 
 (function () {
-        var app = angular.module('app', [
-                'ui.router',
-                'angular-jwt',
-                'oc.lazyLoad',
-                'ngMaterial',
-                'ngMessages',
-                'factories',
-                'loginmodule',
-                'homemodule',
-                'ngMdIcons'
-        ]);
+	var app = angular.module('app', [
+		'ui.router',
+		'angular-jwt',
+		'oc.lazyLoad',
+		'ngMaterial',
+		'ngMessages',
+		'factories',
+		'loginmodule',
+		'homemodule',
+		'iniciomodule',
+		'ngMdIcons'
+	]);
 
-        var config = function ($stateProvider, $urlRouterProvider, $locationProvider, $ocLazyLoadProvider, $httpProvider) {
+	var config = function ($stateProvider, $urlRouterProvider, $locationProvider, $ocLazyLoadProvider, $httpProvider) {
 
-                $httpProvider.interceptors.push(function ($location, $q, $rootScope, jwtHelper) {
-                        return {
-                                request: function (conf) {
-                                        $rootScope.processing = true;
-                                        var token = localStorage.getItem('token');
-                                        if (token && !jwtHelper.isTokenExpired(token)) {
-                                                console.log("Is valid and is not expired");
-                                                conf.headers.Authorization = 'Bearer ' + token;
-                                                $location.path("/home");
-                                        } else {
-                                                localStorage.removeItem('token');
-                                                $location.path("/login");
-                                        }
-                                        return conf;
-                                }
-                        }
-                });
+		$httpProvider.interceptors.push(function ($location, $q, $rootScope, jwtHelper) {
+			return {
+				request : function (conf) {
+					$rootScope.processing = true;
+					var token = localStorage.getItem('token');
+					if (token && !jwtHelper.isTokenExpired(token)) {
+						console.log("Is valid and is not expired");
+						conf.headers.Authorization = 'Bearer ' + token;
+						$location.path("/inicio");
+					} else {
+						localStorage.removeItem('token');
+						$location.path("/login");
+					}
+					return conf;
+				}
+			}
+		});
 
-                $stateProvider
-                        .state('login', {
-                                url: '/login',
-                                templateUrl: 'partials/login.html',
-                                controller: 'loginController'
-                        })
-                        .state('home', {
-                                url: '/home',
-                                templateUrl: 'partials/home.html',
-                                controller: 'HomeController'
-                        });
+		$stateProvider
+			.state('login', {
+				url         : '/login',
+				templateUrl : 'partials/login.html',
+				controller  : 'loginController'
+			})
+			.state('home', {
+				url         : '/home',
+				templateUrl : 'partials/home.html',
+				controller  : 'HomeController'
+			})
+			.state('inicio', {
+				url         : '/inicio',
+				templateUrl : 'partials/inicio.html',
+				controller  : 'InicioController'
+			});
 
-                $urlRouterProvider.otherwise("/login");
+		$urlRouterProvider.otherwise("/login");
 
-                if (window.history && window.history.pushState) {
-                        $locationProvider.html5Mode({
-                                enabled: true,
-                                requireBase: false
-                        });
-                }
-        };
+		if (window.history && window.history.pushState) {
+			$locationProvider.html5Mode({
+				enabled     : true,
+				requireBase : false
+			});
+		}
+	};
 
-        app.config(config);
+	app.config(config);
 
 
 })();
--- app/js/factories.js
+++ app/js/factories.js
@@ -1,71 +1,71 @@
 'use strict';
 (function () {
-        var factories = angular.module('factories', ['angular-jwt']);
+	var factories = angular.module('factories', ['angular-jwt']);
 
-        factories.factory('httpErrorResponseInterceptor', ['$q', '$location',
-                function ($q, $location) {
-                        return {
-                                request: function (config) {
-                                        config.headers = config.headers || {};
-                                        var token = localStorage.getItem('token');
-                                        if (token) {
-                                                config.headers.Authorization = 'Bearer ' + token;
-                                        }
-                                        return config;
-                                },
-                                response: function (responseData) {
-                                        return responseData;
-                                },
-                                responseError: function error(response) {
-                                        switch (response.status) {
-                                                case 401:
-                                                        $location.path('/login');
-                                                        break;
-                                                case 404:
-                                                        $location.path('/404');
-                                                        break;
-                                        }
+	factories.factory('httpErrorResponseInterceptor', ['$q', '$location',
+		function ($q, $location) {
+			return {
+				request       : function (config) {
+					config.headers = config.headers || {};
+					var token = localStorage.getItem('token');
+					if (token) {
+						config.headers.Authorization = 'Bearer ' + token;
+					}
+					return config;
+				},
+				response      : function (responseData) {
+					return responseData;
+				},
+				responseError : function error(response) {
+					switch (response.status) {
+						case 401:
+							$location.path('/login');
+							break;
+						case 404:
+							$location.path('/404');
+							break;
+					}
 
-                                        return $q.reject(response);
-                                }
-                        };
-                }
-        ]);
+					return $q.reject(response);
+				}
+			};
+		}
+	]);
 
-        factories.factory('Auth', ['$http', function ($http) {
-                function getClaimsFromToken() {
-                        var token = localStorage.getItem('token');
-                        var user = {};
-                        user = jwtHelper.decodeToken(token);
-                        return user;
-                }
+	factories.factory('Auth', ['$http', function ($http) {
+		function getClaimsFromToken() {
+			var token = localStorage.getItem('token');
+			var user = {};
+			user = jwtHelper.decodeToken(token);
+			return user;
+		}
 
-                var tokenClaims = getClaimsFromToken();
+		var tokenClaims = getClaimsFromToken();
 
-                return {
-                        login: function (data, success, error) {
-                                $http.post('/login', data).success(success).error(error)
-                        },
-                        logout: function (success) {
-                                tokenClaims = {};
-                                delete localStorage.removeItem('token');
-                                success();
-                        },
-                        getTokenClaims: function () {
-                                return tokenClaims;
-                        }
-                };
-        }]);
+		return {
+			login          : function (data, success, error) {
+				$http.post('/login', data).success(success).error(error)
+			},
+			logout         : function (success) {
+				tokenClaims = {};
+				delete localStorage.removeItem('token');
+				success();
+			},
+			getTokenClaims : function () {
+				return tokenClaims;
+			}
+		};
+	}]);
 
-        factories.service('getToken', function () {
-                var token = localStorage.getItem('token');
-                return token;
-        });
+	factories.service('getToken', function () {
+		var token = localStorage.getItem('token');
+		return token;
+	});
 
-        factories.factory('getServerName', function () {
-                return 'http://0.0.0.0:9000';
-                //return 'http://172.16.9.154:9000';
-                //return 'https://api.hpclab.unitecnologica.edu.co';
-        });
+	factories.factory('getServerName', function () {
+		//return 'http://0.0.0.0:9000';
+		return 'http://172.16.9.152:9000';
+		//return 'https://api.hpclab.unitecnologica.edu.co';
+	});
 
 })();
